4f1b671e8055fba50e56b7c4ee1d20943f213b63
Mohammed Farid <dev.mohammedfarid@gmail.com>
2024-07-15 12:12:54 +0300

    using next to fetch data without external server

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,14 +1,8 @@
 import { NewsList } from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { getAllNews } from "@/lib/news";
 
 async function NewsPage() {
-  const response = await fetch("http://localhost:8080/news");
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch news");
-  }
-
-  const news = await response.json();
+  const news = getAllNews();
 
   return (
     <div>
